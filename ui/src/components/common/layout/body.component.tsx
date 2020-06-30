fa2edbdd7ec082d9aba5700f9a44819b36084a56
Mariano Aguero <mariano.aguero@altoros.com>
2020-06-30 17:45:25 -0300

    Fix waypoint trigger

--- ui/src/components/common/layout/body.component.tsx
+++ ui/src/components/common/layout/body.component.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 const BodyWrapper = styled.div`
-  margin: 48px auto auto auto;
+  margin: 48px auto 20px auto;
 `
 
 export const Body: React.FC = props => {
