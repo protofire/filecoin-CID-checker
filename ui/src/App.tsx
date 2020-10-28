5c2dd9c1a188b6425cd8777918e1801e7a082885
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2020-10-28 16:08:05 -0300

    (add) protofire logo in a different place

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -5,11 +5,13 @@ import theme from './theme'
 import { GlobalStyle } from './theme/global_style'
 import { Main } from './components/main.component'
 import { BackToTop } from './components/backToTop.component'
+import { GitHubCorners } from './components/gitHubCorners.component'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <GitHubCorners />
       <Main />
       <BackToTop />
     </ThemeProvider>
--- ui/src/components/common/layout/mainWrapper.component.tsx
+++ ui/src/components/common/layout/mainWrapper.component.tsx
@@ -1,23 +1,11 @@
 import React from 'react'
-import styled from 'styled-components'
-import { GitHubCorners } from '../../gitHubCorners.component'
-import { ProtofireLink } from '../../protofireLink.component'
-
-const Scroll = styled.div`
-  overflow: hidden;
-  position: relative;
-`
 
 export const MainWrapper: React.FC = props => {
   const { children, ...restProps } = props
 
   return (
-    <Scroll>
-      <GitHubCorners />
-      <ProtofireLink />
-      <div className="container" {...restProps}>
-        {children}
-      </div>
-    </Scroll>
+    <div className="container" {...restProps}>
+      {children}
+    </div>
   )
 }
