78eb0cdeda67821078611699a6a27f427baa89b7
aliaksandr <plcgi1@gmail.com>
2021-10-05 16:20:21 +0300

    pm2 configs

---
+++ packages/backend/.ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    // First application
+    {
+      name: 'cid-checker-fastify-api',
+      script: 'index.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 4444,
+      },
+      env_dev: {
+        NODE_ENV: 'dev',
+        PORT: 4444,
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 4444,
+      },
+    },
+  ],
+}
