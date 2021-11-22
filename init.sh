d1d6831eb7e56391acf13ccae71843015abf7e6b
Aliaksei Malyshau <aliaksei.malyshau@altoros.com>
2021-11-22 16:55:44 +0300

    fix: Update init.sh

--- init.sh
+++ init.sh
@@ -10,7 +10,7 @@ echo "--- Pull last updates from GitHub ---"
 echo "-------------------------------------"
 echo ""
 
-cd /home/ubuntu/CID-Checker-filecoin-tools/filecoin-CID-checker
+cd /home/ubuntu/cid-checker/filecoin-CID-checker
 
 git pull
 
@@ -18,9 +18,9 @@ echo "-------------------------------------------------------"
 echo "--- Build Docker images: frontend, backend, watcher ---"
 echo "-------------------------------------------------------"
 
-docker build -t cid-checker-frontend:$(cat /home/ubuntu/CID-Checker-filecoin-tools/filecoin-CID-checker/packages/frontend/version.txt) -f Dockerfile.frontend .
-docker build -t cid-checker-backend:$(cat /home/ubuntu/CID-Checker-filecoin-tools/filecoin-CID-checker/packages/frontend/version.txt) -f Dockerfile.backend .
-docker build -t cid-checker-watcher:$(cat /home/ubuntu/CID-Checker-filecoin-tools/filecoin-CID-checker/packages/frontend/version.txt) -f Dockerfile.watcher .
+docker build -t cid-checker-frontend:$(cat ./packages/frontend/version.txt) -f Dockerfile.frontend .
+docker build -t cid-checker-backend:$(cat ./packages/frontend/version.txt) -f Dockerfile.backend .
+docker build -t cid-checker-watcher:$(cat ./packages/frontend/version.txt) -f Dockerfile.watcher .
 
 echo "-----------------------------"
 echo "------ Build completed! -----"
