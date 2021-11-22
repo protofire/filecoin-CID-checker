c1cdb6d5e31715d91060f4f16dcb535012ae2c38
Aliaksei Malyshau <aliaksei.malyshau@altoros.com>
2021-11-22 12:31:18 +0300

    fix: Add init.sh script and delete GitHub Actions workflow

---
+++ init.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+echo "----------------------------------------------------"
+echo "--- Init script for CID Checker (filecoin.tools) ---"
+echo "----------------------------------------------------"
+
+echo ""
+echo "-------------------------------------"
+echo "--- Pull last updates from GitHub ---"
+echo "-------------------------------------"
+echo ""
+
+cd /home/ubuntu/CID-Checker-filecoin-tools/filecoin-CID-checker
+
+git pull
+
+echo "---------------------------"
+echo "--- Stop docker-compose ---"
+echo "---------------------------"
+
+docker-compose down
+
+echo "-------------------------------------------------------"
+echo "--- Build Docker images: frontend, backend, watcher ---"
+echo "-------------------------------------------------------"
+
+docker build -t cid-checker-frontend:$(cat /home/ubuntu/CID-Checker-filecoin-tools/filecoin-CID-checker/packages/frontend/version.txt) -f Dockerfile.frontend .
+docker build -t cid-checker-backend:$(cat /home/ubuntu/CID-Checker-filecoin-tools/filecoin-CID-checker/packages/frontend/version.txt) -f Dockerfile.backend .
+docker build -t cid-checker-watcher:$(cat /home/ubuntu/CID-Checker-filecoin-tools/filecoin-CID-checker/packages/frontend/version.txt) -f Dockerfile.watcher .
+
+echo "-----------------------------"
+echo "------ Build completed! -----"
+echo "-----------------------------"
+echo "--- Docker Compose me up! ---"
+echo "-----------------------------"
+
+docker-compose up -d
+
+echo "----------------------"
+echo "--- Cleanup Docker ---"
+echo "----------------------"
+
+docker system prune -f
+
+echo ""
+echo "--------------------"
+echo "All steps completed!"
+echo "--------------------"
+echo ""
