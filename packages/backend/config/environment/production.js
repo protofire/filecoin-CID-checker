1aadd00734113381acfa58267e8ceac392aab5a1
aliaksandr <plcgi1@gmail.com>
2022-04-13 08:58:00 +0300

    prod changes

--- packages/backend/config/environment/production.js
+++ packages/backend/config/environment/production.js
@@ -3,8 +3,8 @@ const env = process.env
 const dbOptions = {
   uri: env.CID_DB_CONNECTIONSTRING,
   options: {
-    tls: true,
-    tlsCAFile: env.CID_DB_CA_FILE,
+    // tls: true,
+    // tlsCAFile: env.CID_DB_CA_FILE,
     dbName: env.CID_DB_NAME,
     auth: {
       user: env.CID_DATABASE_USER,
