02b53ec0ab93d68f734f68a62e56ddf5c068185a
aliaksandr <plcgi1@gmail.com>
2022-04-13 08:29:49 +0300

    updated dev env

--- packages/backend/config/environment/dev.js
+++ packages/backend/config/environment/dev.js
@@ -1,4 +1,12 @@
+const env = process.env
+
 module.exports = {
+  db: {
+    uri: env.CID_DB_CONNECTIONSTRING,
+    options: {
+      dbName: env.CID_DB_NAME,
+    },
+  },
   logging: {
     level: 'debug',
     timestamp: () => `,"timestamp":"${new Date(Date.now()).toISOString()}"`,
