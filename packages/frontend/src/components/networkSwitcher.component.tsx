5e7324ad0a3535a29e0232ae00c8786f55446038
Alex <plcgi1@gmail.com>
2023-03-10 14:37:59 +0300

    Dev (#206)
    * Add cid-checker for hyperspace
    
    * Update buildspec environment
    
    * ga tag for hyperspace
    
    * wallwby helm removed
    
    * updated build version
    
    * be.increased HEIGHT_ALARM_ALLOW_DIFF to 160
    
    * Update buildspec.yaml
    
    * Increase allowed diff
    
    * add variable ONLY_COMPRESSED to values s3SortCaching
    
    * Feat/network switcher (#203)
    
    * network switcher
    
    * network switcher - small fix
    
    * Feat/network switcher (#203) (#204)
    
    * network switcher
    
    * network switcher - small fix
    
    * fe.define network from host
    
    * ci version 0.1.29
    
    ---------
    
    Co-authored-by: Ales Dumikau <ales.dumikau@protofire.io>
    Co-authored-by: lazavikmaria <lazavikmaria@gmail.com>
    Co-authored-by: lazavikmaria <115006984+lazavikmaria@users.noreply.github.com>
    Co-authored-by: Dmitrii Kotelkin <19859885+koteld@users.noreply.github.com>
    Co-authored-by: Arsenii Petrovich <23522179+ArseniiPetrovich@users.noreply.github.com>
    Co-authored-by: Ales Dumikau <ales.dumikau@altoros.com>
    Co-authored-by: dumikau <44052182+dumikau@users.noreply.github.com>

---
+++ packages/frontend/src/components/networkSwitcher.component.tsx
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components';
+import { networks } from '../config/networks'
+import { parseQuery } from '../utils/urls'
+import { INetwork } from '../utils/types';
+
+const DropdownWrapper = styled.div`
+  position: relative;        
+`
+
+const SelectedWrapper = styled.div`
+  font-family: ${(props) => props.theme.fonts.fontFamily};
+  font-size: ${(props) => props.theme.fonts.defaultSize};
+  cursor: pointer;
+  color: #f0f8ff;
+  background: radial-gradient(circle, rgba(207, 224, 255, 0.62) 1px, transparent 1px) repeat-x;
+`
+
+const OptionWrapper = styled.li`
+    margin: 0;
+    padding: 0;
+    color: inherit;
+    &:hover  {
+      background: #42c1ca;
+    }
+`
+
+const MenuWrapper = styled.ul`
+    position: absolute;
+  
+    list-style-type: none;
+    margin: 5px 0;
+    padding: 1px;
+  
+    border: 1px solid grey;
+    width: 120px;
+`
+
+const AWrapper = styled.a`
+  padding: 3px;
+  width: 100%;
+  color: white;
+`
+
+interface DropDownItemOpts {
+  network: INetwork
+  onClick: () => void
+}
+
+const DropdowItem = (opts: DropDownItemOpts) => {
+  return <OptionWrapper onClick={opts.onClick}>
+    <AWrapper href={`${opts.network.url}?network=${opts.network.id}`}
+       rel="noopener noreferrer"
+       target="_blank">
+      {opts.network.label}
+    </AWrapper>
+  </OptionWrapper>
+}
+
+export const NetworkSwitcher = () => {
+  const [open, setOpen] = useState(false);
+  const [network, setNetwork] = useState(networks[0]);
+
+  useEffect(
+    () => {
+      const parsedQuery = parseQuery()
+      let found
+      if(parsedQuery && parsedQuery.hasOwnProperty('network')) {
+        found = networks.find(n => n.id === parsedQuery.network)
+      }
+      if(found) {
+        setNetwork(found)
+        return
+      }
+      const arr = window.location.host.split(/\./)
+      let networkFromHost = 'mainnet'
+      if (arr.length === 3) {
+        networkFromHost = arr[0]
+      }
+      found = networks.find(n => n.id === networkFromHost)
+      if(found) {
+        setNetwork(found)
+      }
+    },
+    []
+  )
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  const options = networks.filter(n => n.id !== network.id)
+
+  const onClick = () => {
+    setOpen(!open);
+  }
+
+  return (
+    <DropdownWrapper>
+      <SelectedWrapper onClick={handleOpen}>{network.label}</SelectedWrapper>
+      {open ? (
+        <MenuWrapper>
+          {
+            options.map(n => {
+              return <DropdowItem onClick={onClick} key={n.id} network={n} />
+            })
+          }
+        </MenuWrapper>
+      ) : null}
+    </DropdownWrapper>
+  )
+}
+
---
+++ packages/frontend/src/config/networks.ts
@@ -0,0 +1,19 @@
+import { INetwork } from '../utils/types';
+
+export const networks: INetwork[] = [
+  {
+    id: 'mainnet',
+    url: 'https://filecoin.tools',
+    label: 'Mainnet',
+  },
+  {
+    id: 'calibration',
+    url: 'https://calibration.filecoin.tools',
+    label: 'Calibration',
+  },
+  {
+    id: 'hyperspace',
+    url: 'https://hyperspace.filecoin.tools',
+    label: 'Hyperspace',
+  },
+]
